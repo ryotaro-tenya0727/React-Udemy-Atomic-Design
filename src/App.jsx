49e7e99b0de0bf980eaf01b9bdd380878565dc80
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2022-01-12 05:15:31 +0900

    【追加】templatesの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+
 import "./styles.css";
 
 const user = {
@@ -17,12 +21,14 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>ほにたん</PrimaryButton>
-      <SecondaryButton>うるむ</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>ほにたん</PrimaryButton>
+        <SecondaryButton>うるむ</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
---
+++ src/components/atoms/layout/Footer.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const SFooter = styled.footer`
+  background-color: #11999e;
+  color: #fff;
+  text-align: center;
+  padding: 8px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
+
+export const Footer = () => {
+  return <SFooter>&copy; 2021 ryotaro Inc.</SFooter>;
+};
